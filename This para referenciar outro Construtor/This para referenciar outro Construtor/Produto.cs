dff65e325e3f6d89f649d12007ec2eefcfbca3d2
Carlos Felipe Santos <ccfelipe08@gmail.com>
2025-08-29 16:58:40 -0300

    .

---
+++ This para referenciar outro Construtor/This para referenciar outro Construtor/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Produto
+{
+    public string nome;
+    public double preco;
+    public int quantidade;
+
+    // Construtor 1: sem parâmetros
+    public Produto()
+    {
+        quantidade = 10;
+        Console.WriteLine("Construtor Produto() chamado: quantidade = 10");
+    }
+
+    // Construtor 2: nome e preço
+    public Produto(string nome, double preco) : this()
+    {
+        this.nome = nome;
+        this.preco = preco;
+        Console.WriteLine($"Construtor Produto(nome, preco) chamado: nome = {nome}, preco = {preco}");
+    }
+
+    // Construtor 3: nome, preço e quantidade
+    public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+    {
+        this.quantidade = quantidade;
+        Console.WriteLine($"Construtor Produto(nome, preco, quantidade) chamado: quantidade = {quantidade}");
+    }
+
+    public override string ToString()
+    {
+        return $"Produto: {nome}, Preço: {preco}, Quantidade: {quantidade}";
+    }
+}
+
+
