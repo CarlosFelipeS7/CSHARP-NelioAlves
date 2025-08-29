65d11c29db4a3f047f341431770b23c62327136f
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2025-08-29 10:57:25 -0300

    Trabalhando com Construtores

---
+++ Construtor/Construtor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtor
+{
+    //Construtor é uma operacao especial da classe, que executa no momento da instanciacao do objeto
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
