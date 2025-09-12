ecd6288f5bc448585b676768c8cc87c7cfe2861d
Carlos Felipe Santos <ccfelipe08@gmail.com>
2025-09-12 16:44:51 -0300

    UPCASTING E DOWNCASTING

--- Herança/Herança/Entities/SavingsAccount.cs
+++ Herança/Herança/Entities/SavingsAccount.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Herança.Entities
 {
-    internal class SavingsAccount
+     class SavingsAccount: Account
     {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            :base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
     }
 }
