f8f84c3df0773a1646fc0ff8ed1fb7b8651d101f
Carlos Felipe Santos <ccfelipe08@gmail.com>
2025-09-25 16:37:09 -0300

    .

--- 30_LocadoraVeiculos_Sem_Interface/30_LocadoraVeiculos_Sem_Interface/Program.cs
+++ 30_LocadoraVeiculos_Sem_Interface/30_LocadoraVeiculos_Sem_Interface/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using _30_LocadoraVeiculos_Sem_Interface.Entities;
+using _30_LocadoraVeiculos_Sem_Interface.Services;
 
 namespace _30_LocadoraVeiculos_Sem_Interface
 {
@@ -17,14 +18,26 @@ namespace _30_LocadoraVeiculos_Sem_Interface
             Console.WriteLine("Car model: ");
             string model = Console.ReadLine();
             Console.WriteLine( "Pickup (dd/MM/yyyy hh:mm" );
-            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM//yyyy HH:mm", CultureInfo.InvariantCulture);
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM//yyyy hh:mm", CultureInfo.InvariantCulture);
             Console.WriteLine("Return (dd/MM/yyyy hh:mm");
-            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM//yyyy HH:mm", CultureInfo.InvariantCulture);
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM//yyyy hh:mm", CultureInfo.InvariantCulture);
 
+            Console.WriteLine("Enter price per hour");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter price per day");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            
             //instanciando objetos com base nos valores informados
-
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
 
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("Invoice: ");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
--- 30_LocadoraVeiculos_Sem_Interface/30_LocadoraVeiculos_Sem_Interface/Services/RentalService.cs
+++ 30_LocadoraVeiculos_Sem_Interface/30_LocadoraVeiculos_Sem_Interface/Services/RentalService.cs
@@ -12,6 +12,8 @@ namespace _30_LocadoraVeiculos_Sem_Interface.Services
         public double PricePerHour { get; private set; } //adicionando restricao, so posso obter, nao posso modificar em outras classes
         public double PricePerDay { get; private set; }
 
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
         public RentalService(double pricePerHour, double pricePerDay)
         {
             PricePerHour = pricePerHour;
@@ -20,7 +22,22 @@ namespace _30_LocadoraVeiculos_Sem_Interface.Services
 
         public void ProcessInvoice(CarRental carRental)
         {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
 
+            double basicPayment = 0.0;
+            if(duration.TotalHours<= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            //instanciando
+            carRental.Invoice = new Invoice(basicPayment, tax);
         }
 
     }
