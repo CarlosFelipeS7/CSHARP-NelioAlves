08753c31f2f112d12f220209de04f4e8f4826879
Carlos Felipe Santos <ccfelipe08@gmail.com>
2025-09-25 16:21:53 -0300

    Exercicios de Fixação

---
+++ 30_LocadoraVeiculos_Sem_Interface/30_LocadoraVeiculos_Sem_Interface/Services/RentalService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _30_LocadoraVeiculos_Sem_Interface.Entities;
+
+namespace _30_LocadoraVeiculos_Sem_Interface.Services
+{
+    internal class RentalService
+    {
+        public double PricePerHour { get; private set; } //adicionando restricao, so posso obter, nao posso modificar em outras classes
+        public double PricePerDay { get; private set; }
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+
+        }
+
+    }
+}
