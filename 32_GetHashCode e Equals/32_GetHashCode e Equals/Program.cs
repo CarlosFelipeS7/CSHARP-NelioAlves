0ca11a6d4996ae3aa25723057c65d6191308116d
Carlos Felipe Santos <ccfelipe08@gmail.com>
2025-09-29 15:07:51 -0300

    GetHashCode e Equals

--- 32_GetHashCode e Equals/32_GetHashCode e Equals/Program.cs
+++ 32_GetHashCode e Equals/32_GetHashCode e Equals/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _32_GetHashCode_e_Equals.Entities;
 
 namespace _32_GetHashCode_e_Equals
 {
@@ -10,21 +11,35 @@ namespace _32_GetHashCode_e_Equals
     {
         static void Main(string[] args)
         {
-            //Ambas sao operacoes da classe Object
-            //Para comparar se um objeto é igual a outro
+            //Pelo que entendi o hashcode é uma busca mais generica, porem mais rapida
+            //O equals é mais lenta, porem 100%
 
-            //Equals: lento, resposta 100%, retorna true ou false
+            //Analogia: objeto:livro, o hashcode seria localizar as pratileiras rapido
+            //o equals seria ver o nome do livro e autor
+
+            //Entao utiliza os 2
+
+            //Equals:  retorna se os valores dos objetos sao iguais
             string a = "Maria";
             string b = "Alex";
-            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.Equals(b)); //false
+            string e = "Lucas";
+            string f = "Lucas";
+            Console.WriteLine(e.Equals(f)); //retorna true, pois os objetos tem o mesmo valor, mesmo sendo difrentes
 
-            //GetHashCode: rapido, porem nao é 100%, retorna um codigo a partir das infos do objeto
+            //GetHashCode: retorna um codigo a partir das infos do objeto
             string c = "Pedro";
             string d = "Pedro";
-            Console.WriteLine(c.GetHashCode());  
+            Console.WriteLine(c.GetHashCode());
             Console.WriteLine(d.GetHashCode());
 
-            
+
+            Client cliente1 = new Client { Email = "maria@gmail.com", Name = "Maria" };
+           Client cliente2 = new Client { Name = "Alex", Email = "alex@gmail.com"};
+
+            Console.WriteLine(cliente1.Equals(cliente2));
+            Console.WriteLine(cliente1.GetHashCode());
+            Console.WriteLine(cliente2.GetHashCode());
             
 
 
