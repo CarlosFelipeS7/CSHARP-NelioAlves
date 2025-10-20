2c1a2121006a0016fc5e96c0d24c77d90a8dda23
Carlos Felipe Santos <ccfelipe08@gmail.com>
2025-10-20 14:12:39 -0300

    Other entities and second migration

---
+++ SalesWebMVC/SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+
+        public Departament Departament { get; set; }
+
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        //colecoes nao inclui nos construtores
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Departament departament)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            //usando o linq (where) para filtrar as vendas, ai utilizo a expressao lambda que sera a condicao
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+
+        }
+
+
+    }
+    
+}
