eba0cc3a2103e23d37c7842362b4ae2211f62fa2
Carlos Felipe Santos <ccfelipe08@gmail.com>
2025-10-22 15:35:56 -0300

    Operacoes Assincronas

--- SalesWebMVC/SalesWebMVC/Services/DepartamentService.cs
+++ SalesWebMVC/SalesWebMVC/Services/DepartamentService.cs
@@ -2,6 +2,7 @@
 using SalesWebMVC.Models;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMVC.Services
 {
@@ -14,9 +15,9 @@ namespace SalesWebMVC.Services
             _context = context;
         }
         
-        public List<Departament> FindAll() 
+        public async Task<List<Departament>> FindAllAsync() 
         {
-            return _context.Departament.OrderBy(x => x.Name).ToList();
+            return await _context.Departament.OrderBy(x => x.Name).ToListAsync();
         }
 
     }
